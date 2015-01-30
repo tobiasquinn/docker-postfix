6fcfc59d06140eddaffc34f66f6cc21e3dc67ce5
Tobias Quinn <tobias@tobiasquinn.com>
2015-01-30 16:07:32 +0000

    run mailserver on port 420 and build tobias/postfix image

--- assets/install.sh
+++ assets/install.sh
@@ -60,12 +60,12 @@ if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/ce
   postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname *.key)
   chmod 400 /etc/postfix/certs/*.*
   # /etc/postfix/master.cf
-  postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
-  postconf -P "submission/inet/syslog_name=postfix/submission"
-  postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
-  postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
-  postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
-  postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
+  postconf -M 420/inet="420   inet   n   -   n   -   -   smtpd"
+  postconf -P "420/inet/syslog_name=postfix/420"
+  postconf -P "420/inet/smtpd_tls_security_level=encrypt"
+  postconf -P "420/inet/smtpd_sasl_auth_enable=yes"
+  postconf -P "420/inet/milter_macro_daemon_name=ORIGINATING"
+  postconf -P "420/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
 fi
 
 #############
